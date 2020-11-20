cb8439fa7e849c34090174e6065d7d0e2519b030
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2020-11-20 10:17:07 -0300

    Method post for user login

--- GerenciadorCondominios/Controllers/UsuariosController.cs
+++ GerenciadorCondominios/Controllers/UsuariosController.cs
@@ -1,10 +1,13 @@
-﻿using GerenciadorCondominios.DAL.Interfaces;
+﻿using GerenciadorCondominios.BLL.Models;
+using GerenciadorCondominios.DAL.Interfaces;
 using GerenciadorCondominios.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -37,8 +40,70 @@ namespace GerenciadorCondominios.Controllers
         [HttpPost]
         public async Task<IActionResult> Registro(RegistroViewModel model, IFormFile foto)
         {
-            await 
-            return View();
+            // Certificando dados validos
+            if (ModelState.IsValid)
+            {
+                if(foto != null)
+                {
+                    string diretorioPasta = Path.Combine(_webHostEnvironment.WebRootPath, "Imagens");
+                    // Incluindo a foto com valor único no nome (Guid)
+                    string nomeFoto = Guid.NewGuid().ToString() + foto.FileName;
+
+                    using(FileStream fileStream = new FileStream(Path.Combine(diretorioPasta, nomeFoto), FileMode.Create))
+                    {
+                        await foto.CopyToAsync(fileStream);
+                        model.Foto = "~/Imagens/" + nomeFoto;
+                    }
+                }
+
+                Usuario usuario = new Usuario();
+                IdentityResult usuarioCriado;
+
+                // Criar administrador se for o primeiro cadastro
+                if(_usuarioRepositorio.VerificarSeExisteRegistro() == 0)
+                {
+                    usuario.UserName = model.Nome;
+                    usuario.CPF = model.CPF;
+                    usuario.Email = model.Email;
+                    usuario.PhoneNumber = model.Telefone;
+                    usuario.Foto = model.Foto;
+                    usuario.PrimeiroAcesso = false;
+                    usuario.Status = StatusConta.Aprovado;
+
+                    usuarioCriado = await _usuarioRepositorio.CriarUsuario(usuario, model.Senha);
+
+                    if (usuarioCriado.Succeeded)
+                    {
+                        await _usuarioRepositorio.IncluirUsuarioEmFuncao(usuario, "Administrador");
+                        await _usuarioRepositorio.LogarUsuario(usuario, false);
+                        return RedirectToAction("Index", "Usuarios");
+                    }
+                }
+
+                usuario.UserName = model.Nome;
+                usuario.CPF = model.CPF;
+                usuario.Email = model.Email;
+                usuario.PhoneNumber = model.Telefone;
+                usuario.Foto = model.Foto;
+                usuario.PrimeiroAcesso = true;
+                usuario.Status = StatusConta.Analisando;
+
+                usuarioCriado = await _usuarioRepositorio.CriarUsuario(usuario, model.Senha);
+
+                if (usuarioCriado.Succeeded)
+                {
+                    return View("Analise", usuario.UserName);
+                }
+                else
+                {
+                    foreach(IdentityError erro in usuarioCriado.Errors)
+                    {
+                        ModelState.AddModelError("", erro.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }

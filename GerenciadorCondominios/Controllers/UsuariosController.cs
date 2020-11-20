76b88c62d863eec3c69554254e858dffb0352476
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2020-11-20 13:20:02 -0300

    Login Usuarios parte 3

--- GerenciadorCondominios/Controllers/UsuariosController.cs
+++ GerenciadorCondominios/Controllers/UsuariosController.cs
@@ -107,7 +107,7 @@ namespace GerenciadorCondominios.Controllers
         }
 
         [HttpGet]
-        public async IActionResult Login()
+        public async Task<IActionResult> Login()
         {
             if (User.Identity.IsAuthenticated)
                 await _usuarioRepositorio.DeslogarUsuario();
@@ -165,6 +165,13 @@ namespace GerenciadorCondominios.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _usuarioRepositorio.DeslogarUsuario();
+            return RedirectToAction("Login");
         }
 
         public IActionResult Analise(string nome)

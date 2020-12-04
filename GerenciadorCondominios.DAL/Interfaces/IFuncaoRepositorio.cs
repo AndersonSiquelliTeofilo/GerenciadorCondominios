20173267b4f5af706291b8dfabc8bede3c873d90
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2020-12-03 21:30:46 -0300

    Funções

--- GerenciadorCondominios.DAL/Interfaces/IFuncaoRepositorio.cs
+++ GerenciadorCondominios.DAL/Interfaces/IFuncaoRepositorio.cs
@@ -2,10 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.DAL.Interfaces
 {
     public interface IFuncaoRepositorio : IRepositorioGenerico<Funcao>
     {
+        Task AdicionarFuncao(Funcao funcao);
+
+        new Task Atualizar(Funcao funcao);
     }
 }
---
+++ GerenciadorCondominios/Controllers/FuncoesController.cs
@@ -0,0 +1,74 @@
+﻿using GerenciadorCondominios.BLL.Models;
+using GerenciadorCondominios.DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.Controllers
+{
+    public class FuncoesController : Controller
+    {
+        private readonly IFuncaoRepositorio _funcaoRepositorio;
+
+        public FuncoesController(IFuncaoRepositorio funcaoRepositorio)
+        {
+            _funcaoRepositorio = funcaoRepositorio;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _funcaoRepositorio.PegarTodos());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Descricao,Id,Name,NormalizedName,ConcurrencyStamp")] Funcao funcao)
+        {
+            if (ModelState.IsValid)
+            {
+                await _funcaoRepositorio.AdicionarFuncao(funcao);
+                TempData["NovoRegistro"] = $"Função {funcao.Name} adicionada";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcao);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            Funcao funcao = await _funcaoRepositorio.PegarPeloId(id);
+            if(funcao == null)
+            {
+                return NotFound();
+            }
+            return View(funcao);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("Descricao,Id,Name,NormalizedName,ConcurrencyStamp")] Funcao funcao)
+        {
+            if (ModelState.IsValid)
+            {
+                await _funcaoRepositorio.Atualizar(funcao);
+                TempData["Atualizacao"] = $"Função {funcao.Name} atualizada";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcao);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Delete(string id)
+        {
+            await _funcaoRepositorio.Excluir(id);
+            TempData["Exclusao"] = $"Função excluída";
+            return Json("Função Excluída");
+        }
+    }
+}
